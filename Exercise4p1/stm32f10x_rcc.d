c7374eea66571d52fd29013382955ac62bc54868
RobGah <47264017+RobGah@users.noreply.github.com>
2020-07-09 08:26:27 -0400

    renamed exercises to make contents clear

--- Exercise4p1/stm32f10x_rcc.d
+++
@@ -1,4 +0,0 @@
-stm32f10x_rcc.o: ../Library/stm32f10x_rcc.c ../Library/stm32f10x_rcc.h \
- ../Library/stm32f10x.h \
- C:\Users\Robert\Documents\DiscoveringSTM32\STM32F10x_StdPeriph_Lib_V3.5.0/Libraries/CMSIS/CM3/CoreSupport/core_cm3.h \
- ../Library/system_stm32f10x.h ../stm32f10x_conf.h
