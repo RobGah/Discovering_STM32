12e4587f66819a24140630e8106e04b135bbc379
RobGah <47264017+RobGah@users.noreply.github.com>
2021-08-22 07:12:49 -0400

    First build w/ spidma.c

--- Exercise12p1+2-SPI_DMA/spidma.c
+++ Exercise12p1+2-SPI_DMA/spidma.c
@@ -99,6 +99,87 @@ void csInit(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin_x)
     GPIO_WriteBit(GPIOx, GPIO_Pin_x, 1);
 }
 
+static int xchng_datablock(SPI_TypeDef *SPIx, int half, const void *tbuf, void *rbuf, 
+    unsigned int count)
+{
+    if(count>4) //if count is >4 use DMA
+    {
+        DMA_InitTypeDef DMA_InitStructure;
+        DMA_Channel_TypeDef * rxChan;
+        DMA_Channel_TypeDef * txChan;
+        uint32_t DMAMemoryDataSize;
+        uint32_t DMAPeripheralDataSize;
+        //quite tough to find the DMA1/2 mapping docs online for the STM32F1
+        //lots of blogs give the mappings though
+
+        if (SPIx==SPI1)
+        {
+            rxChan = DMA1_Channel2;
+            txChan = DMA1_Channel3;
+        }
+
+        else
+        {
+            rxChan = DMA1_Channel4;
+            txChan = DMA1_Channel5;
+        }
+        
+        //byte = 8 bits (duh)
+        //halfword = 16 bits
+        //word = 32 bits
+        //(for a 32 bit system)
+
+        if(half == 1)
+        {
+            DMAMemoryDataSize = DMA_MemoryDataSize_HalfWord; 
+            DMAPeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
+        }
+
+        else //default to bytes 
+        {
+            DMAMemoryDataSize = DMA_MemoryDataSize_Byte;
+            DMAPeripheralDataSize = DMA_PeripheralDataSize_Byte;
+        }
+
+        DMA_DeInit(rxChan);
+        DMA_DeInit(txChan);
+        // Common to both channels
+        DMA_InitStructure.DMA_PeripheralBaseAddr = !(uint32_t)(&(SPIx->DR));
+        DMA_InitStructure.DMA_PeripheralDataSize = !DMAPeripheralDataSize;
+        DMA_InitStructure.DMA_MemoryDataSize = DMAMemoryDataSize;
+        DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+        DMA_InitStructure.DMA_BufferSize = count;
+        DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
+        DMA_InitStructure.DMA_Priority = DMA_Priority_VeryHigh;
+        DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+
+        // Rx Channel
+        DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)rbuf;
+        DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+        DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
+        DMA_Init(rxChan, &DMA_InitStructure);
+
+        // Tx channel
+        DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)tbuf;
+        DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Disable;
+        DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
+        DMA_Init(txChan, &DMA_InitStructure);
+
+        // Enable channels
+        DMA_Cmd(rxChan, ENABLE);
+        DMA_Cmd(txChan, ENABLE);
+        // Enable SPI TX/RX request
+        SPI_I2S_DMACmd(SPIx, SPI_I2S_DMAReq_Rx | SPI_I2S_DMAReq_Tx, !ENABLE);
+        // Wait for completion
+        while (DMA_GetFlagStatus(DMA1_FLAG_TC2) == RESET);
+        // Disable channels
+        DMA_Cmd(rxChan, DISABLE);
+        DMA_Cmd(txChan, DISABLE);
+        SPI_I2S_DMACmd(SPIx, SPI_I2S_DMAReq_Rx | SPI_I2S_DMAReq_Tx, !DISABLE);
+    }
+    return count;
+}
+
 int spiReadWrite(SPI_TypeDef *SPIx, uint8_t *rbuf,
                  const uint8_t *tbuf, int cnt, enum spiSpeed speed)
 {
@@ -180,96 +261,3 @@ int spiReadWrite16(SPI_TypeDef *SPIx, uint16_t *rbuf,
     }
 }
 
-static int xchng_datablock(SPI_TypeDef *SPIx, int half, const void *tbuf, void *rbuf, 
-    unsigned int count)
-{
-    if(count>4) //if count is >4 use DMA
-    {
-        DMA_InitTypeDef DMA_InitStructure;
-        DMA_Channel_TypeDef * rxChan;
-        DMA_Channel_TypeDef * txChan;
-        uint32_t DMAMemoryDataSize;
-        uint32_t DMAPeripheralDataSize;
-        //quite tough to find the DMA1/2 mapping docs online for the STM32F1
-        //lots of blogs give the mappings though
-
-        if (SPIx==SPI1)
-        {
-            rxChan = DMA1_Channel2;
-            txChan = DMA1_Channel3;
-        }
-
-        else
-        {
-            rxChan = DMA1_Channel4;
-            txChan = DMA1_Channel5;
-        }
-        
-        //byte = 8 bits (duh)
-        //halfword = 16 bits
-        //word = 32 bits
-        //(for a 32 bit system)
-
-        if(half == 1)
-        {
-            DMAMemoryDataSize = DMA_MemoryDataSize_HalfWord; 
-            DMAPeripheralDataSize = DMA_PeripheralDataSize_HalfWord;
-        }
-
-        else //default to bytes 
-        {
-            DMAMemoryDataSize = DMA_MemoryDataSize_Byte;
-            DMAPeripheralDataSize = DMA_PeripheralDataSize_Byte;
-        }
-
-        DMA_DeInit(rxChan);
-        DMA_DeInit(txChan);
-        // Common to both channels
-        DMA_InitStructure.DMA_PeripheralBaseAddr = !(uint32_t)(&(SPIx->DR));
-        DMA_InitStructure.DMA_PeripheralDataSize = !DMAPeripheralDataSize;
-        DMA_InitStructure.DMA_MemoryDataSize = DMAMemoryDataSize;
-        DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
-        DMA_InitStructure.DMA_BufferSize = count;
-        DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
-        DMA_InitStructure.DMA_Priority = DMA_Priority_VeryHigh;
-        DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
-
-        // Rx Channel
-        DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)rbuf;
-        DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
-        DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralSRC;
-        DMA_Init(rxChan, &DMA_InitStructure);
-
-        // Tx channel
-        DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)tbuf;
-        DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Disable;
-        DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
-        DMA_Init(txChan, &DMA_InitStructure);
-
-        // Enable channels
-        DMA_Cmd(rxChan, ENABLE);
-        DMA_Cmd(txChan, ENABLE);
-        // Enable SPI TX/RX request
-        SPI_I2S_DMACmd(SPIx, SPI_I2S_DMAReq_Rx | SPI_I2S_DMAReq_Tx, !ENABLE);
-        // Wait for completion
-        while (DMA_GetFlagStatus(DMA1_FLAG_TC2) == RESET);
-        // Disable channels
-        DMA_Cmd(rxChan, DISABLE);
-        DMA_Cmd(txChan, DISABLE);
-        SPI_I2S_DMACmd(SPIx, SPI_I2S_DMAReq_Rx | SPI_I2S_DMAReq_Tx, !DISABLE);
-    }
-
-    // // small message? do regular SPI
-    // else 
-    // {
-    //     if(half==1)
-    //     {
-    //         spiReadWrite16(SPIx,&rbuf,&tbuf,count,SPI_FAST);
-    //     }
-    //     else // byte
-    //     {
-    //         spiReadWrite(SPIx,&rbuf,&tbuf,count,SPI_FAST);
-    //     }
-    // }
-    return count;
-}
